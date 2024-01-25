3b0f335a245f81c5547d063079493cc4aae8f3f0
EAG <mataramosmiguel@gmail.com>
2024-01-25 20:47:31 +0100

    lokura god con semaforos

--- app/src/main/java/com/example/seminario3di/Ejercicio2.kt
+++ app/src/main/java/com/example/seminario3di/Ejercicio2.kt
@@ -7,82 +7,98 @@ import android.os.CountDownTimer
 import android.widget.ImageView
 import androidx.core.animation.doOnEnd
 import com.example.seminario3di.databinding.ActivityEjercicio2Binding
+import java.util.concurrent.Semaphore
 
 class Ejercicio2 : AppCompatActivity() {
     private lateinit var binding: ActivityEjercicio2Binding
+    private val semaforo1 = Semaphore(1)
+    private val semaforo2 = Semaphore(1)
+    private val semaforo3 = Semaphore(1)
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityEjercicio2Binding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
         binding.carta1.setOnClickListener {
-            animacionGirar(binding.carta1,R.drawable.carta1)
-            var ct = object : CountDownTimer(2400, 1000) {
-                override fun onTick(millisUntilFinished: Long) {
+            if (semaforo1.tryAcquire()){
+                animacionGirar(binding.carta1,R.drawable.carta1)
+                var ct = object : CountDownTimer(2400, 1000) {
+                    override fun onTick(millisUntilFinished: Long) {
 
-                }
+                    }
 
-                override fun onFinish() {
-                    animacionGirarReversa(binding.carta1,R.drawable.parte_trasera)
+                    override fun onFinish() {
+                        animacionGirarReversa(binding.carta1,R.drawable.parte_trasera)
+                        semaforo1.release()
+                    }
                 }
+                ct.start()
+
             }
 
-            ct.start()
         }
 
         binding.carta2.setOnClickListener {
-            animacionGirar(binding.carta2,R.drawable.carta2)
-            var ct = object : CountDownTimer(2400, 1000) {
-                override fun onTick(millisUntilFinished: Long) {
+            if (semaforo2.tryAcquire()){
+                animacionGirar(binding.carta2,R.drawable.carta2)
+                var ct = object : CountDownTimer(2400, 1000) {
+                    override fun onTick(millisUntilFinished: Long) {
 
+                    }
+                    override fun onFinish() {
+                        animacionGirarReversa(binding.carta2,R.drawable.parte_trasera)
+                        semaforo2.release()}
                 }
-                override fun onFinish() {
-                    animacionGirarReversa(binding.carta2,R.drawable.parte_trasera)                }
+
+                ct.start()
             }
 
-            ct.start()
+
         }
 
         binding.carta3.setOnClickListener {
-            animacionGirar(binding.carta3,R.drawable.carta3)
-            var ct = object : CountDownTimer(2400, 1000) {
-                override fun onTick(millisUntilFinished: Long) {
+            if (semaforo3.tryAcquire()){
+                animacionGirar(binding.carta3,R.drawable.carta3)
+                var ct = object : CountDownTimer(2400, 1000) {
+                    override fun onTick(millisUntilFinished: Long) {
 
+                    }
+                    override fun onFinish() {
+                        animacionGirarReversa(binding.carta3,R.drawable.parte_trasera)
+                        semaforo3.release()}
                 }
-                override fun onFinish() {
-                    animacionGirarReversa(binding.carta3,R.drawable.parte_trasera)                }
+
+                ct.start()
             }
 
-            ct.start()
         }
-
-
-
 
     }
 
     fun animacionGirar(carta: ImageView, draw: Int){
         val medioGiro = ObjectAnimator.ofFloat(carta, "rotationY", 0f, 90f)
-        medioGiro.duration = 500
+        medioGiro.duration = 400
         medioGiro.start()
 
         medioGiro.doOnEnd {
             carta.setImageResource(draw) // Aquí es donde cambias la imagen
             val segundoMedioGiro = ObjectAnimator.ofFloat(carta, "rotationY", 90f, 180f)
-            segundoMedioGiro.duration = 500
+            segundoMedioGiro.duration = 400
             segundoMedioGiro.start()
         }
     }
 
     fun animacionGirarReversa(carta: ImageView, imagen: Int){
         val medioGiro = ObjectAnimator.ofFloat(carta, "rotationY", 180f, 90f)
-        medioGiro.duration = 500
+        medioGiro.duration = 300
         medioGiro.start()
 
         medioGiro.doOnEnd {
             carta.setImageResource(imagen) // Aquí es donde cambias la imagen
             val segundoMedioGiro = ObjectAnimator.ofFloat(carta, "rotationY", 90f, 0f)
-            segundoMedioGiro.duration = 500
+            segundoMedioGiro.duration = 300
             segundoMedioGiro.start()
         }
     }
